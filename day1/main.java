d63c2155920740d8b9ee0001be18a46b100a2a96
Ashishtripathi29 <ashishtripathipara@gmail.com>
2023-07-22 06:35:44 +0530

    day1

---
+++ day1/main.java
@@ -0,0 +1,26 @@
+package day1;
+
+import java.util.Scanner;
+
+class main {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!"); 
+        // System.out.println("ahsih tripathi");
+
+        // for(int i=0;i<10;i++){
+        //     System.out.print(i+" ");
+        // }
+        int k=0;
+        if(k==5){
+            System.out.println(k);
+        }
+        else{
+            System.out.println("k panch nahi hai");
+        }
+        
+    try (Scanner sc = new Scanner(System.in)) {
+        String userInput = sc.nextLine();
+         System.out.println(userInput);
+    }
+    }
+}
