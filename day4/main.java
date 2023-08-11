c87174f7b2a9c95c532f8a91aac6919be605fad0
Ashishtripathi29 <ashishtripathipara@gmail.com>
2023-08-11 09:12:01 +0530

    string

---
+++ day4/main.java
@@ -0,0 +1,6 @@
+package day4;
+public class main{
+public static void main(String[] args) {
+    System.out.println("Enter three number to sum");
+}
+}
---
+++ day5_String/main.java
@@ -0,0 +1,22 @@
+package day5_String;
+
+import java.util.Scanner;
+
+public class main {
+     public static void main(String [] arg){
+        // string cover in this tutorials
+
+        String name=new String("Ashish");
+        // System.out.println(name);
+        // System.out.printf("%s is my name",name);
+        // String name2=name;
+        // System.out.println(name2.charAt(0));
+        // Scanner sc=new Scanner(System.in);
+        int num=56;
+        float fnum=3.45f;
+        System.out.println(num);
+        System.out.printf("num is %d and fnum is %8.2f",num,fnum);
+
+
+     }
+}
